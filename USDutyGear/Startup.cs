d6735ec365687fa7e7a4ca4b1b1aa9e1c13629a4
Steven <sgarc22@gmail.com>
2016-05-09 22:03:04 -0700

    Initial commit

---
+++ USDutyGear/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(USDutyGear.Startup))]
+namespace USDutyGear
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
