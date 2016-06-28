b880b66eb81c373cbb7d5691e44353f618726693
Steven <sgarc22@gmail.com>
2016-06-28 11:45:20 -0700

    styled the home page more

---
+++ USDutyGear/obj/Release/AspnetCompileMerge/Source/js/__product.js
@@ -0,0 +1,19 @@
+﻿var ctrl = {};
+
+ctrl.init = function (vm) {
+    ctrl.vm = vm;
+
+    ko.applyBindings(ctrl.vm);
+};
+
+// ready function
+$(function () {
+    // parse product name from URL
+    //var tokens = window.location.split('/');
+    //var productName = tokens[tokens.length - 1];
+
+    //ctrl.init(productName);
+});
+
+//productController.init();
+
