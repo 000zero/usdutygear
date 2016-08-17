2e25d8b22b170a77ac57137e920a1552029dcf42
Steven Garcia <Steven.Garcia@cybercoders.net>
2016-08-17 16:32:07 -0700

    added promotion model

---
+++ USDutyGear.Core/Models/ProductPromotion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace USDutyGear.Core.Models
+{
+    public class ProductPromotion
+    {
+        public string Id { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int Percentage { get; set; }
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
