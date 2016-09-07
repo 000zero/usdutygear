3a41e1484f30d32b596d35b8927e71267ab278b5
Steven <sgarc22@gmail.com>
2016-09-07 00:40:40 -0700

    more work on the cart price calculation

--- USDutyGear/Controllers/CartApiController.cs
+++ USDutyGear/Controllers/CartApiController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text;
 using System.Linq;
 using System.Web.Http;
 using System.Net.Http;
@@ -19,6 +20,7 @@ namespace USDutyGear.Controllers
         public HttpResponseMessage GetCartViewModel(CartViewModel cart)
         {
             // cart comes in with only the models and quantities, need to set the rest
+            var total = 0.0m;
             foreach (var item in cart.Items)
             {
                 var tokens = item.Model.Split('-');
@@ -28,29 +30,34 @@ namespace USDutyGear.Controllers
                 var adjustments = Products.GetProductAdjustmentsByModel(product.Model);
 
                 // calculate the price of this particular object
-                item.Price = CalculateProductPrice(item.Model);
-                // TODO: now set the title
-                // find finish adjustment
+                item.Price = GetProductPrice(item.Model, product, adjustments);
+                item.Title = BuildProductTitle(item.Model, product, adjustments);
+                item.Total = item.Price * item.Quantity;
 
-                // get the data from the db
-
-                // set the data for the item
+                total += item.Total;
             }
-
-            // go through the cart
-
-            // get the product object from the model number
 
             return Request.CreateResponse(HttpStatusCode.OK, cart);
         }
 
-        private decimal CalculateProductPrice(string model)
+        private string BuildProductTitle(string model, Product product, List<ProductAdjustment> adjustments)
         {
-            var productModel = model.Substring(0, model.IndexOf('-'));
+            var match = product.ModelTemplate.Match(model);
+            var title = new StringBuilder(product.Title);
+            var adjustmentModel = match.Groups[ProductAdjustmentTypes.Finish].Value;
 
+            title = title.Replace("{Finish}", GetProductAdjustment(adjustments, match, ProductAdjustmentTypes.Finish).Name);
+            title = title.Replace("{Size}", GetProductAdjustment(adjustments, match, ProductAdjustmentTypes.Size).Name);
+            title = title.Replace("{Snap}", GetProductAdjustment(adjustments, match, ProductAdjustmentTypes.Snap).Name);
+            if (match.Groups["Package"].Success)
+                title = title.Append($" ({GetProductAdjustment(adjustments, match, ProductAdjustmentTypes.Package).Name})");
+
+            return title.ToString();
+        }
+
+        private decimal GetProductPrice(string model, Product product, List<ProductAdjustment> adjustments)
+        {
             // get the product model
-            var product = Products.GetProductByModel(productModel);
-            var adjustments = Products.GetProductAdjustmentsByModel(product.Model);
             var match = product.ModelTemplate.Match(model);
 
             // set the base price
@@ -58,27 +65,27 @@ namespace USDutyGear.Controllers
 
             // adjust for finish
             if (adjustments.Any(x => x.Name == ProductAdjustmentTypes.Finish))
-                price += GetPriceAdjustment(adjustments, match, ProductAdjustmentTypes.Finish);
+                price += GetProductAdjustment(adjustments, match, ProductAdjustmentTypes.Finish).PriceAdjustment;
             // adjust for size
             if (adjustments.Any(x => x.Name == ProductAdjustmentTypes.Size))
-                price += GetPriceAdjustment(adjustments, match, ProductAdjustmentTypes.Size);
+                price += GetProductAdjustment(adjustments, match, ProductAdjustmentTypes.Size).PriceAdjustment;
             // adjust for snap
             if (adjustments.Any(x => x.Name == ProductAdjustmentTypes.Snap))
-                price += GetPriceAdjustment(adjustments, match, ProductAdjustmentTypes.Snap);
+                price += GetProductAdjustment(adjustments, match, ProductAdjustmentTypes.Snap).PriceAdjustment;
             // adjust for package
             if (adjustments.Any(x => x.Name == ProductAdjustmentTypes.Package))
-                price += GetPriceAdjustment(adjustments, match, ProductAdjustmentTypes.Package);
+                price += GetProductAdjustment(adjustments, match, ProductAdjustmentTypes.Package).PriceAdjustment;
 
             return price;
         }
 
-        private static decimal GetPriceAdjustment(List<ProductAdjustment> adjustments, Match match, string adjustmentType)
+        private static ProductAdjustment GetProductAdjustment(List<ProductAdjustment> adjustments, Match match, string adjustmentType)
         {
             // extract the adjustment model from the model string via regex
             var adjustmentModel = match.Groups[adjustmentType.ToLower()].Value;
             // get the price adjustment by the adjustment model and type
             var adjustment = adjustments.First(x => x.Model == adjustmentModel && x.Name == adjustmentType);
-            return adjustment.PriceAdjustment;
+            return adjustment;
         }
     }
 }
